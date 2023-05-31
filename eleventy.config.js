ced5de156e4696cedd1706b96d59cf2e031f6322
James Basoo <jamesbasoo@gmail.com>
2023-05-31 22:29:22 +0100

    disable heading anchor thing

--- eleventy.config.js
+++ eleventy.config.js
@@ -85,18 +85,18 @@ module.exports = function(eleventyConfig) {
 	});
 
 	// Customize Markdown library settings:
-	eleventyConfig.amendLibrary("md", mdLib => {
-		mdLib.use(markdownItAnchor, {
-			permalink: markdownItAnchor.permalink.ariaHidden({
-				placement: "after",
-				class: "header-anchor",
-				symbol: "#",
-				ariaHidden: false,
-			}),
-			level: [1,2,3,4],
-			slugify: eleventyConfig.getFilter("slugify")
-		});
-	});
+	// eleventyConfig.amendLibrary("md", mdLib => {
+	// 	mdLib.use(markdownItAnchor, {
+	// 		permalink: markdownItAnchor.permalink.ariaHidden({
+	// 			placement: "after",
+	// 			class: "header-anchor",
+	// 			symbol: "#",
+	// 			ariaHidden: false,
+	// 		}),
+	// 		level: [1,2,3,4],
+	// 		slugify: eleventyConfig.getFilter("slugify")
+	// 	});
+	// });
 	
 	eleventyConfig.setServerOptions({
     port: 8113
