e2c31603467b717cdec2b901bcdcbc2ce46b0385
James Basoo <jamesbasoo@gmail.com>
2023-06-01 17:21:18 +0100

    New home content. Various style tweaks

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,9 @@
 // @TODO: move to bundle or edge function to get/set cookie before load
-const themeSwitcherInputs = document.querySelectorAll('.theme-switcher input')
 
-themeSwitcherInputs[0].checked = true;
-
-themeSwitcherInputs.forEach((element) => {
-	element.addEventListener('change', (event) => {
+if(typeof themeSwitcher === 'undefined') {
+	const themeSwitcher = document.querySelector('.theme-switcher');
+	
+	themeSwitcher.addEventListener('change', (event) => {
 		document.querySelector('html').dataset.theme = event.target.value;
 	});
-});
+}
