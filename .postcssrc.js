3cc7146ddc2a935fd097214f144f4157b8832129
James Basoo <jamesbasoo@gmail.com>
2023-09-27 14:20:36 +0100

    layout updates

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,9 +3,20 @@ const breakpoints = require('./config/breakpoints')
 module.exports = {
 	plugins: {
 		'postcss-import': {},
-		'postcss-simple-vars': {variables: breakpoints},
+		'postcss-simple-vars': { variables: breakpoints },
 		'postcss-nesting': {},
 		'autoprefixer': {},
-		'cssnano': {}
+		'cssnano': {},
+		// '@fullhuman/postcss-purgecss': {
+		// 	content: [
+		// 		'./_site/**/*.html',
+		// 		'./_site/**/*.css',
+		// 		'./_site/**/*.js'
+		// 	],
+		// 	css: ['./site/css/main.css'],
+		// 	rejected: true,
+		// 	variables: true,
+		// 	fontFace: true
+		// }
 	}
 }
