f7c153b60037696b8e53c303588402cc35f9efba
James Basoo <jamesbasoo@gmail.com>
2023-10-30 15:43:59 +0000

    WIP web component

--- public/js/components/carousel.js
+++ public/js/components/carousel.js
@@ -1,10 +1,39 @@
 class JABCarousel extends HTMLElement {
+    static styles = `
+        :root {
+            background: blue;
+        }
+
+        * {
+            all: unset;
+        }
+
+        ul {
+            overflow: auto;
+            display: flex;
+            gap: var(--spacing, .5rem);
+        }
+
+        li {
+            flex: 1 0 var(--item-block-size, 30%);
+            background: red;
+        }
+    `;
+
     constructor() {
         super();
+
+        this.styles = new CSSStyleSheet();
+        this.styles.replaceSync(this.constructor.styles)
+        this.template = document.querySelector("template");
+
+        const shadow = this.attachShadow({ mode: "open" });
+        shadow.adoptedStyleSheets = [this.styles];
+        shadow.appendChild(this.template.content.cloneNode(true));
     }
 
     connectedCallback() {
-        console.log(this, 'jab-carousel');
+
     }
 }
 
