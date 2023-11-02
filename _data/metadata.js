9bc3fe645b5bddbc73a71c0907370a568dcb0bf6
James Basoo <jamesbasoo@gmail.com>
2023-11-02 16:18:32 +0000

    Update meta data

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "James Basoo - Frontend Developer",
+	title: "James Basoo - Front-End Developer",
 	url: "https://jamesbasoo.com/",
 	language: "en",
-	description: "Front-end developer",
+	description: "Front-end developer from the UK with over a decade of experience in accessibility, multiple frameworks, and e-commerce platforms.",
 	author: {
 		name: "James Basoo",
-		email: "jamesbasoo@gmailo.com",
+		email: "jamesbasoo@gmail.com",
 		url: "https://jamesbasoo.com/"
 	}
 }
