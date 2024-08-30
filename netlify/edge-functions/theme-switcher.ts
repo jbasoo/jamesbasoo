bc02b7fb4f918d27cc51b132bd3b3ab3cd99f63c
James Basoo <jamesbasoo@gmail.com>
2024-08-30 19:08:58 +0100

    filter out non html response types

--- netlify/edge-functions/theme-switcher.ts
+++ netlify/edge-functions/theme-switcher.ts
@@ -2,16 +2,18 @@ import type { Config } from "@netlify/edge-functions";
 import { HTMLRewriter } from "https://ghuc.cc/worker-tools/html-rewriter/index.ts";
 
 export default async function handler(request: Request, context: Context) {
-	const theme = context.cookies.get("theme");
-
-	if(!theme) return;
   const response = await context.next();
-  const rewriter = new HTMLRewriter()
-    .on("html", {
-      element: (element) => {
-        element.setAttribute('data-theme', theme);
-      },
-    });
+	const theme = context.cookies.get("theme");
+	const type = response.headers.get('content-type') as string;
+
+	if(!theme || !type.startsWith('text/html')) return;
+
+  const rewriter = new HTMLRewriter().on("html", {
+		element: (element) => {
+			element.setAttribute('data-theme', theme);
+		}
+	});
+
   return rewriter.transform(response);
 }
 
@@ -20,6 +22,7 @@ export const config: Config = {
   excludedPath: [
 		"/*.css",
 		"/*.js",
+		"/*.mjs",
 		"/*.svg",
 		"/*.jpg",
 		"/*.woff2",
