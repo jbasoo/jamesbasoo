bfb62e51f5ad0c58e639ba90dcff8e1708ddb076
James Basoo <jamesbasoo@gmail.com>
2024-09-03 10:29:56 +0100

    remove meta theme

--- public/js/components/theme-switcher.js
+++ public/js/components/theme-switcher.js
@@ -3,12 +3,6 @@ import Cookies from '/js-cookie/js.cookie.min.mjs'
 const themeSwitcher = document.querySelector('.theme-switcher');
 const defaultTheme = (window.matchMedia("(prefers-color-scheme: dark)").matches ? 'holodeck' : 'light');
 const currentTheme = Cookies.get('theme');
-const metaTheme = document.createElement('meta');
-metaTheme.name = "theme-color";
-
-function setMetaTheme() {
-	metaTheme.content = getComputedStyle(document.documentElement).getPropertyValue("--c-accent");
-}
 
 if(!currentTheme) {
 	Cookies.set('theme', defaultTheme);
@@ -23,11 +17,6 @@ if(themeSwitcher) {
 		if(event.target.tagName === 'BUTTON') {
 			document.querySelector('html').dataset.theme = event.target.value;
 			Cookies.set('theme', event.target.value);
-
-			setMetaTheme();
 		}
 	});
 }
-
-document.head.appendChild(metaTheme);
-setMetaTheme();
